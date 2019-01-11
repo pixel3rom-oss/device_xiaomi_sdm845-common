88367e59218867cba62a83b9d9a31ec87b1081c9
Bruno Martins <bgcngm@gmail.com>
2019-01-11 22:46:14 +0000

    sdm845-common: Add support for equuleus and perseus
    Change-Id: Ie13bb76bd338f1f971eb366962677e287f0c0471

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter beryllium dipper polaris ursa,$(TARGET_DEVICE)),)
+ifneq ($(filter beryllium dipper equuleus perseus polaris ursa,$(TARGET_DEVICE)),)
     include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
